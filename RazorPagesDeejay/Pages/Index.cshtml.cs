fd865056b6eb6910499a8d75ecfc823fff8fb523
David Browning <david.a.browning@gmail.com>
2025-06-25 20:14:46 +0200

    Show Album names on Index.

--- RazorPagesDeejay/Pages/Index.cshtml.cs
+++ RazorPagesDeejay/Pages/Index.cshtml.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesDeejay.Data;
+using RazorPagesDeejay.Models;
 
 namespace RazorPagesDeejay.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _applicationDbContext;
+        public IEnumerable<Album> Albums { get; set; } = new List<Album>();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext applicationDbContext)
         {
             _logger = logger;
+            _applicationDbContext = applicationDbContext;
         }
 
         public void OnGet()
         {
-
+            Albums = _applicationDbContext.Albums;
         }
     }
 }
